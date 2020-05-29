f34532443f97d88cd34fe7c463641f76ee083cab
Jesper <jezper@hotmail.dk>
2020-05-29 08:23:03 +0200

    Client og server async opdateret  til at være mere forstålig

---
+++ Server client async/Client og server Async/Client.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client_og_server_Async
+{
+    class Client
+    {
+        string Brugernavn = "";
+        public Client()
+        {
+            // Skriv den port og IPaddress vi ville connecte til som står ved serveren
+            TcpClient client = new TcpClient();
+            int port = 69;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+            NetworkStream stream = client.GetStream();
+            ReciveMeseege(stream);
+
+            // Funktionen til at lave sit brugernavnet 
+            BrugerJoinerServer(stream);
+
+            // Her skriver vi en besked som bliver sent til stream.read ved ser
+            Console.WriteLine("Du kan begynde at skrive en besked");
+            
+            while (true)
+            {
+                string Besked = Console.ReadLine();
+                string Besked1 = Brugernavn + ": " + Besked;
+
+                byte[] buffer = Encoding.UTF8.GetBytes(Besked1);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+        }
+
+        // Læser de beskeder der kommer gennem streamen
+        public async void ReciveMeseege(NetworkStream stream)
+        {
+            byte[] bytes = new byte[256];
+            while (true)
+            {
+                int messege = await stream.ReadAsync(bytes, 0, bytes.Length);
+                string messegeGained = Encoding.UTF8.GetString(bytes, 0, messege);
+                Console.Write("\n" + messegeGained);
+            }
+        }
+
+        // Functionen til at lave et brugernavn til clienten
+        public string BrugerJoinerServer(NetworkStream stream)
+        {
+            if(Brugernavn == "")
+            {
+               Console.WriteLine("Skriv dit brugernavn");
+               Brugernavn = Console.ReadLine();
+               string Joiner = Brugernavn + " har joined severen";
+
+                byte[] buffer = Encoding.UTF8.GetBytes(Joiner);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+            return Brugernavn;
+        }
+    }
+}
---
+++ Server client async/Client og server Async/Server.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Client_og_server_Async
+{
+    
+    class Server
+    {
+        public List<TcpClient> clients = new List<TcpClient>();
+        
+        public Server()
+        {
+            // Opretter en server 
+            int port = 69;
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            // Activere functionen 'AcceptClient'
+            TcpListener listener = new TcpListener(endPoint);
+            AcceptClient(listener);
+
+            // Skriver en besked som clienter kan se
+            while (true)
+            {
+                string Besked = Console.ReadLine();
+                string Besked1 = "Server: " + Besked;
+                byte[] bytes = Encoding.UTF8.GetBytes(Besked1);
+
+                foreach(TcpClient client in clients)
+                {
+                    client.GetStream().Write(bytes, 0, bytes.Length);
+                }
+            }
+        }
+         
+        // Her viller serveren høre efter hvis der er clienter der joiner serveren
+        public async void AcceptClient(TcpListener listener)
+        {
+            Console.WriteLine("Venter på client");
+            while (true)
+            {
+                listener.Start();
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                clients.Add(client);
+               // Console.WriteLine("En client har joined serveren");
+
+                NetworkStream stream = client.GetStream();
+                ReciveMessages(stream);
+            }
+        }
+
+        // Gør at serveren kan læse beskeder fra clienterne
+        public async void ReciveMessages(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+            while(true)
+            {
+                int BeskedeSendt = await stream.ReadAsync(buffer, 0, buffer.Length);
+                string text = Encoding.UTF8.GetString(buffer, 0, BeskedeSendt);
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
